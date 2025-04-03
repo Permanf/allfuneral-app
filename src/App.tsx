b14ba14225c19b5c9a44d112639ef8d2a0ea4d1b
Permanf <tirkishmyradovperman@gmail.com>
2025-04-03 15:59:23 +0500

    fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Center, Loader } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-// Containers
 const Layout = lazy(() => import("./layouts/layout"));
 const NotFound = lazy(() => import("./pages/404"));
 
@@ -12,17 +11,6 @@ const NotFound = lazy(() => import("./pages/404"));
 const queryClient = new QueryClient();
 
 function App() {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(userLoading());
-  //   let token = GetCookie("access_token_admin");
-  //   if (!token) {
-  //     dispatch(userLoadFailed());
-  //   } else {
-  //     dispatch(loginSuccess(token));
-  //   }
-  // }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
